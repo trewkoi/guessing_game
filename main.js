7d34b1428a8f3602b23ce70ea68aa06d1a00b76f
Gabriel Borsato <119875282+trewkoi@users.noreply.github.com>
2024-01-22 22:12:21 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+let correctGuess = Math.floor(Math.random() * 10)
+
+console.log(correctGuess)
+
+const tries = []
+
+const guess = document.getElementById("inputNumber")
+
+document.getElementById("makeGuess").addEventListener("click", (ev) => {
+    ev.preventDefault()
+
+    if (Number(guess.value) === correctGuess) {
+        tries.push(guess.value)
+        document.querySelector(".screen1").classList.toggle("hide")
+        document.querySelector(".screen2").classList.toggle("hide")
+        
+        document.querySelector(".success").innerText = `Successful in ${tries.length} tries`
+    } else {
+        tries.push(guess.value)
+        guess.value = ""
+    }
+})
+
+document.getElementById("playAgain").addEventListener("click", (ev) => {
+    ev.preventDefault()
+    guess.value = ""
+    document.querySelector(".screen1").classList.toggle("hide")
+    document.querySelector(".screen2").classList.toggle("hide")    
+})
